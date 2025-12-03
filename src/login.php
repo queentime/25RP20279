fc5d974246d3a4fd13b118ebae4c8902b9c9338e
Queen Time <queen_1234@example.com>
2025-12-03 08:35:29 -0800

    login page is created.

---
+++ src/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+
+    <h2>Login Page</h2>
+
+    <form action="login.php" method="POST">
+        <label>Username:</label><br>
+        <input type="text" name="username" required><br><br>
+
+        <label>Password:</label><br>
+        <input type="password" name="password" required><br><br>
+
+        <button type="submit">Login</button>
+    </form>
+
+    <br>
+    <a href="index.php">Back to Home</a>
+
+</body>
+</html>
