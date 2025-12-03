01d5273487f964156b9789c5d82ceb81c4765acd
Queen Time <queen_1234@example.com>
2025-12-03 09:05:23 -0800

    registration page is created.

--- src/registration.php
+++ src/registration.php
@@ -8,8 +8,22 @@
     <h2>Registration Page</h2>
 
     <form action="registration.php" method="POST">
-        <label>Username:</label><br>
-        <input type="text" name="username" required><br><br>
+
+        <label>First Name:</label><br>
+        <input type="text" name="firstname" required><br><br>
+
+        <label>Last Name:</label><br>
+        <input type="text" name="lastname" required><br><br>
+
+        <label>Gender:</label><br>
+        <select name="gender" required>
+            <option value="">-- Select Gender --</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+        </select><br><br>
+
+        <label>Email:</label><br>
+        <input type="email" name="email" required><br><br>
 
         <label>Password:</label><br>
         <input type="password" name="password" required><br><br>
@@ -22,3 +36,4 @@
 
 </body>
 </html>
+
