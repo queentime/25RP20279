89e1cb1fcd519560883061fbb1add0891b329200
Queen Time <queen_1234@example.com>
2025-12-03 01:26:52 -0800

    Index page is created.

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Page</title>
+</head>
+<body>
+
+    <h1>Welcome to My Project</h1>
+
+    <p>
+        <a href="registration.php">Register Here</a>
+    </p>
+
+    <p>
+        <a href="login.php">Login Here</a>
+    </p>
+
+</body>
+</html>
